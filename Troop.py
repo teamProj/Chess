6a4c5a422444dd56ce19b0a800257ec61467b9fe
teamProj <30401168+teamProj@users.noreply.github.com>
2017-07-24 15:09:29 +0700

    Update Troop.py

--- Troop.py
+++ Troop.py
@@ -1,14 +1,26 @@
 import pygame
 
-class Front:
-    def __init__(self, root, color):
+pygame.init()
+pygame.font.init()
+
+class Font:
+    def add_text(root, size, text, color, posx, posy):
+        myfont = pygame.font.SysFont('SimHei', size)
+        surf = myfont.render(text, False, color)
+        root.blit(surf, (posx,posy))
+
+class Front(Font):
+    def __init__(self, root, color, txt_color):
         self.root = root
         self.color = color
+        self.txt_color = txt_color
         self.x = 65
         self.y = 65
 
     def display(self):
         pygame.draw.circle(self.root, self.color, (self.x, self.y), 25)
+        self.add_text(self.root, 10, '卒', self.txt_color, self.x, self.y)
+        
 
 class Gunner:
     def __init__(self):
